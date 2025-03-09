b5a70e538848b5d9c0879346bd2cfeae38c9c670
Madhusha Prasad <madushaprasad21@gmail.com>
2025-03-09 18:18:36 +0530

    feat: add MovieModel class for movie data representation

---
+++ Models/MovieModel.cs
@@ -0,0 +1,11 @@
+namespace BlazorMoviewReview.Models
+{
+  public class MovieModel
+  {
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+    public string Review { get; set; } = string.Empty;
+  }
+}
